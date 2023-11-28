188a064ce3d9f35a342ce84ad72452809dd29afe
VasoBaramidze824 <vasilbaramidze2228@gmail.com>
2023-11-28 18:39:01 +0100

    load app with tabbar

--- MoviesAppColaboration/SceneDelegate.swift
+++ MoviesAppColaboration/SceneDelegate.swift
@@ -18,7 +18,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        window?.rootViewController = ViewController()
+        window?.rootViewController = createTabbarController()
         window?.makeKeyAndVisible()
     }
 
@@ -50,6 +50,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
+    func createTabbarController() -> UITabBarController {
+        
+        let tabbarController = UITabBarController()
 
+        let homeVC = UIViewController()
+        homeVC.tabBarItem.image = UIImage(systemName: "house.fill")
+        
+        let profileVC = UIViewController()
+        profileVC.tabBarItem.image = UIImage(systemName: "person.crop.circle")
+        
+        tabbarController.setViewControllers([homeVC, profileVC], animated: true)
+        tabbarController.tabBar.tintColor = .white
+        
+        return tabbarController
+    }
 }
+
 
