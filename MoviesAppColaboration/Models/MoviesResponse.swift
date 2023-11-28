4be007afceeb1d1193a667585111ffbc0cbcaaa6
VasoBaramidze824 <vasilbaramidze2228@gmail.com>
2023-11-28 19:12:46 +0100

    add movie models

---
+++ MoviesAppColaboration/Models/MoviesResponse.swift
@@ -0,0 +1,12 @@
+//
+//  MoviesResponse.swift
+//  MoviesAppColaboration
+//
+//  Created by Baramidze on 28.11.23.
+//
+
+import Foundation
+
+struct MoviesResponse: Decodable {
+    let results: [Movie]
+}
