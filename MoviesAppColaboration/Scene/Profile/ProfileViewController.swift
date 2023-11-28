f02aa52eb23856d7e6f52ac3785da4283119062a
NanaJimsheleishvili <jimsheleishvili.nana@tbcacademy.ge>
2023-11-28 21:54:55 +0400

    Create profile page
    Create profile page

---
+++ MoviesAppColaboration/Scene/Profile/ProfileViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ProfileViewController.swift
+//  MoviesAppColaboration
+//
+//  Created by Nana Jimsheleishvili on 28.11.23.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
